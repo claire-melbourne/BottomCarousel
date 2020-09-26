d1fed39a49542899d6a5c4ec1514b48a7da2e7f7
Jeff Wang <jeffwang92@gmail.com>
2020-09-25 22:49:43 -0400

    implemented GET request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const Furniture = require('../database-mongodb/Furniture');
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.get('/relatedFurnitures', (request, response) => {
+    Furniture.find({}, (error, data) => {
+        console.log(error);
+        //console.log(data);
+        response.send(JSON.stringify(data));
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server listening at port: ${port}`);
